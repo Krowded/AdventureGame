45c128ff182ce355e6942fa8948639d497421832
Krowded <overkrowded@hotmail.com>
2014-11-26 13:24:08 +0100

    Tinytinytinychange

--- AdventureGame/AdventureGame/Classes/Main/AdventureGame.cs
+++ AdventureGame/AdventureGame/Classes/Main/AdventureGame.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
-using System;
 using System.Collections.Generic;
 #endregion
 
